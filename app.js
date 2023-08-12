734f859153a4af94f9937313693411b33ec16e15
daxkharris <daxkharris@pop-os.localdomain>
2023-08-11 18:07:10 -0600

    basic setup

---
+++ app.js
@@ -0,0 +1,6 @@
+function showTab(tabId) {
+    document.querySelectorAll('section').forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(tabId).classList.add('active');
+}
