2cc97b03f940d949532f8fdbee18622cad9e6fa4
daxkharris <daxkharris@pop-os.localdomain>
2023-08-15 16:38:04 -0600

    Added some notes

--- Games/chess.js
+++ Games/chess.js
@@ -1,13 +1,15 @@
+// learn id's!!!!!!!
+
 const board = [
     // 0 represents an empty square, other values represent pieces
-    [5, 4, 3, 2, 6, 3, 4, 5], // 8th row (black pieces)
+    [4, 2, 3, 6, 5, 3, 2, 4], // 8th row (black pieces)
     [1, 1, 1, 1, 1, 1, 1, 1], // 7th row (black pawns)
     [0, 0, 0, 0, 0, 0, 0, 0], // 6th row (empty)
     [0, 0, 0, 0, 0, 0, 0, 0], // 5th row (empty)
-    [0, 0, 0, 0, 0, 0, 0, 0], // 4th row (empty)
+    [0, 0, 0, 0, 0, 0, 0, 7], // 4th row (empty)
     [0, 0, 0, 0, 0, 0, 0, 0], // 3rd row (empty)
-    [7, 7, 7, 7, 7, 7, 7, 7], // 2nd row (white pawns)
-    [11, 10, 9, 8, 12, 9, 10, 11], // 1st row (white pieces)
+    [7, 7, 7, 7, 7, 7, 7, 0], // 2nd row (white pawns)
+    [10, 8, 9, 12, 11, 9, 8, 10], // 1st row (white pieces)
 ];
 
 let selectedPiece = null;
@@ -20,6 +22,7 @@ function renderBoard() {
         const tr = document.createElement('tr');
         for (let col = 0; col < 8; col++) {
             const td = document.createElement('td');
+            boardPointers[row[col]] = td
             if ((row + col) % 2 === 0) {
                 td.classList.add('white');
             } else {
@@ -42,7 +45,7 @@ function renderBoard() {
 function getPieceSymbol(piece) {
     const symbols = ['&#9817;', '&#9816;', '&#9815;', '&#9814;', '&#9813;', '&#9812;',
         '&#9823;', '&#9822;', '&#9821;', '&#9820;', '&#9819;', '&#9818;'];
-    return symbols[Math.abs(piece) - 1];
+    return symbols[piece - 1];
 }
 
 function selectSquare(row, col) {
@@ -55,7 +58,8 @@ function selectSquare(row, col) {
         }
     } else {
         // Attempting to move the selected piece
-        const { row: fromRow, col: fromCol } = selectedPiece;
+        const{ row: fromRow, col: fromCol } = selectedPiece;
+        
         if (isValidMove(fromRow, fromCol, row, col)) {
             // Move the piece
             board[row][col] = board[fromRow][fromCol];
@@ -69,21 +73,7 @@ function selectSquare(row, col) {
 function highlightValidMoves(fromRow, fromCol, piece) {
     // Here, you'd implement the logic to determine valid moves for the selected piece
     // For simplicity, let's just highlight the available adjacent squares (not considering piece rules)
-    const possibleMoves = [
-        { row: fromRow - 1, col: fromCol },
-        { row: fromRow + 1, col: fromCol },
-        { row: fromRow, col: fromCol - 1 },
-        { row: fromRow, col: fromCol + 1 },
-    ];
 
-    for (const move of possibleMoves) {
-        const { row, col } = move;
-        if (isValidSquare(row, col)) {
-            const td = document.querySelector(`#chessboard tr:nth-child(${row + 1}) td:nth-child(${col + 1})`);
-            td.classList.add('valid-move');
-            td.addEventListener('click', () => selectSquare(row, col));
-        }
-    }
 }
 
 function isValidMove(fromRow, fromCol, toRow, toCol) {
